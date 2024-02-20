342129d8fc5cf4070dd824a6e558f793a5a2f8e9
HeberthDev <heberthdeveloper@gmail.com>
2024-02-20 08:43:35 -0300

    Two-way Data Binding

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
@@ -14,7 +15,8 @@ import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensam
     CriarPensamentoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
