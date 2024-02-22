fc55889c47ec2e59e6c794a52847b5a5868de9b8
HeberthDev <heberthdeveloper@gmail.com>
2024-02-22 08:27:14 -0300

    Implementando interfaces

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
-import { ListarPensamentoComponent } from './componentes/listar-pensamento/listar-pensamento.component';
-import { PensamentoComponent } from './componentes/pensamento/pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
 
 @NgModule({
   declarations: [
